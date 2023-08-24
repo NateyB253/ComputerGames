570a4a91a77b61821179c23b205d68253c85fe3f
Nathan Breshears <nbreshears24@gmail.com>
2023-08-24 15:09:51 -0400

    Initial Check In

--- CPGames Cleanup.py
+++ CPGames Cleanup.py
@@ -1,10 +1,32 @@
 # Data Cleanup for CSV File
+from datetime import datetime
 import csv
+
+dates = []
 COL_NAME = 0
 COL_DEVELOPER = 1
+COL_PRODUCER = 2
+COL_GENRE = 3 
+COL_OP_SYSTEM = 4
+COL_DATE = 5
 
-with open('ComputerGames.csv', 'r') as file:
+with open('ComputerGames.csv') as file:
     reader = csv.reader(file)
     for row in reader:
-        print(row[COL_NAME], row[COL_DEVELOPER])
+        dates.append(row[COL_DATE])
+
+for date in dates:
+    if len(date) == 4:
+        x = date
+        date_object = datetime.strptime(x, "%Y")
+        print(date_object.strftime("%m-%d-%Y"))
+
+
+"""with open('ComputerGamesOutput.csv', 'w') as file:
+    writer = csv.writer(file)
+    for row in writer:
+    
+"""
+
+
 

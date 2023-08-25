1bc0de4bc3e81419ccf2e5278700c2699c68be19
Nathan Breshears <nbreshears24@gmail.com>
2023-08-25 13:18:48 -0400

    Created try and except condition to iterate through for loop and reformat date columns to US date format

--- CPGames Cleanup.py
+++ CPGames Cleanup.py
@@ -1,6 +1,8 @@
 # Data Cleanup for CSV File
 from datetime import datetime
 import csv
+import re
+import pandas as pd
 
 dates = []
 COL_NAME = 0
@@ -9,24 +11,32 @@ COL_PRODUCER = 2
 COL_GENRE = 3 
 COL_OP_SYSTEM = 4
 COL_DATE = 5
+date_regex = r'\d{1,2}-[a-zA-Z]{3}-(?:\d{2}|\d{4})'
+date_object = None
 
 with open('ComputerGames.csv') as file:
     reader = csv.reader(file)
     for row in reader:
         dates.append(row[COL_DATE])
 
+
 for date in dates:
-    if len(date) == 4:
-        x = date
-        date_object = datetime.strptime(x, "%Y")
+    try:
+        date_object = datetime.strptime(date, "%d-%b-%y")
+    except ValueError:
+        try:
+            date_object = datetime.strptime(date, "%d-%b-%Y")
+        except ValueError:
+             if len(date) == 4:
+                date_object = datetime.strptime(date, "%Y")
+    if date_object != None:
         print(date_object.strftime("%m-%d-%Y"))
 
-
-"""with open('ComputerGamesOutput.csv', 'w') as file:
+'''
+with open('ComputerGamesOutput.csv', 'w') as file:
     writer = csv.writer(file)
     for row in writer:
-    
-"""
+'''   
 
 
 

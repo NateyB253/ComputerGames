7de1187927c8160faf5537de7f34335b8eca6d0f
Devin Breshears <dbreshears@users.noreply.github.com>
2023-08-25 09:10:33 -0700

    Fixed Creating Output CSV File

--- CPGames Cleanup.py
+++ CPGames Cleanup.py
@@ -2,28 +2,30 @@
 from datetime import datetime
 import csv
 
-outrow = []
+outrows = []
 COL_NAME = 0
 COL_DEVELOPER = 1
 COL_PRODUCER = 2
 COL_GENRE = 3 
 COL_OP_SYSTEM = 4
 COL_DATE = 5
-
+i = 0
 
 with open('ComputerGames.csv') as file:
     reader = csv.reader(file)
     
     for row in reader:
-        outrow.extend(row)
-        if len(outrow[COL_DATE]) == 4:
-            x = outrow[COL_DATE]
+        outrows.append(row)
+        if len(outrows[i][COL_DATE]) == 4:
+            x = outrows[i][COL_DATE]
             date_object = datetime.strptime(x, "%Y")
-            outrow[COL_DATE] = date_object.strftime("%m-%d-%Y")
+            outrows[i][COL_DATE] = date_object.strftime("%m-%d-%Y")
+        i+=1
 
-with open('ComputerGamesOutput.csv', 'wb') as file:
-    writer = csv.writer(file, lineterminator='\n')
-    writer.writerow(outrow)
+with open('ComputerGamesOutput.csv', 'w') as file:
+    for row in outrows:
+        writer = csv.writer(file, lineterminator='\n')
+        writer.writerow(row)
 
 """ for row in reader:
    print(row[COL_NAME], row[COL_DEVELOPER], row[COL_PRODUCER], row[COL_GENRE], row[COL_OP_SYSTEM], row[COL_DATE])

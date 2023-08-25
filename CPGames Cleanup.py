3476b3cc0d45066cb04f4a1c8d5268093261717a
Devin Breshears <dbreshears@users.noreply.github.com>
2023-08-25 08:50:07 -0700

    First crack at creating the output CSV file

--- CPGames Cleanup.py
+++ CPGames Cleanup.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 import csv
 
-dates = []
+outrow = []
 COL_NAME = 0
 COL_DEVELOPER = 1
 COL_PRODUCER = 2
@@ -10,23 +10,21 @@ COL_GENRE = 3
 COL_OP_SYSTEM = 4
 COL_DATE = 5
 
+
 with open('ComputerGames.csv') as file:
     reader = csv.reader(file)
-    for row in reader:
-        dates.append(row[COL_DATE])
-
-for date in dates:
-    if len(date) == 4:
-        x = date
-        date_object = datetime.strptime(x, "%Y")
-        print(date_object.strftime("%m-%d-%Y"))
-
-
-"""with open('ComputerGamesOutput.csv', 'w') as file:
-    writer = csv.writer(file)
-    for row in writer:
     
+    for row in reader:
+        outrow.extend(row)
+        if len(outrow[COL_DATE]) == 4:
+            x = outrow[COL_DATE]
+            date_object = datetime.strptime(x, "%Y")
+            outrow[COL_DATE] = date_object.strftime("%m-%d-%Y")
+
+with open('ComputerGamesOutput.csv', 'wb') as file:
+    writer = csv.writer(file, lineterminator='\n')
+    writer.writerow(outrow)
+
+""" for row in reader:
+   print(row[COL_NAME], row[COL_DEVELOPER], row[COL_PRODUCER], row[COL_GENRE], row[COL_OP_SYSTEM], row[COL_DATE])
 """
-
-
-

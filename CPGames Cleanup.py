fdbe2bac073db2b2de4a06b7d4e74b24d6666756
Nathan Breshears <nbreshears24@gmail.com>
2023-08-31 11:28:06 -0400

    Completed reformatting the dates column to represent US date format

--- CPGames Cleanup.py
+++ CPGames Cleanup.py
@@ -1,42 +1,45 @@
 # Data Cleanup for CSV File
 from datetime import datetime
 import csv
-import re
-import pandas as pd
 
-dates = []
+outrow = []
 COL_NAME = 0
 COL_DEVELOPER = 1
 COL_PRODUCER = 2
 COL_GENRE = 3 
 COL_OP_SYSTEM = 4
 COL_DATE = 5
-date_regex = r'\d{1,2}-[a-zA-Z]{3}-(?:\d{2}|\d{4})'
 date_object = None
+i=0
 
 with open('ComputerGames.csv') as file:
     reader = csv.reader(file)
     for row in reader:
-        dates.append(row[COL_DATE])
-
-
-for date in dates:
+        outrow.append(row)
+    
+for row in outrow:
+    x = outrow[i][COL_DATE]
     try:
-        date_object = datetime.strptime(date, "%d-%b-%y")
+        date_object = datetime.strptime(x, "%d-%b-%y")
     except ValueError:
         try:
-            date_object = datetime.strptime(date, "%d-%b-%Y")
+            date_object = datetime.strptime(x, "%d-%b-%Y")
         except ValueError:
-             if len(date) == 4:
-                date_object = datetime.strptime(date, "%Y")
+            if len(outrow[i][COL_DATE]) == 4:
+                date_object = datetime.strptime(x, "%Y")
     if date_object != None:
-        print(date_object.strftime("%m-%d-%Y"))
+        outrow[i][COL_DATE] = date_object.strftime("%m-%d-%Y")
+        print(outrow[i][COL_DATE])
+    i+=1
+  
 
-'''
-with open('ComputerGamesOutput.csv', 'w') as file:
-    writer = csv.writer(file)
-    for row in writer:
-'''   
+        
+with open('ComputerGamesOutput.csv', 'w', newline = '') as file:
+    
+    for row in outrow:
+        writer = csv.writer(file)
+        writer.writerow(row)
+
 
 
 
